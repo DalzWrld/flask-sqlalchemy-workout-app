68597fdf3fde07d229465aa8672eccd7165e12ae
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-07-25 23:29:52 +0300

    feat: add schemas

--- schemas.py
+++ schemas.py
@@ -6,3 +6,9 @@ class ExerciseSchema(Schema):
     name = fields.Str(required=True)
     category = fields.Str(required=True)
     equipment_needed = fields.Boolean(required=True)
+
+class WorkoutSchema(Schema):
+    id = fields.Int(dump_only=True)
+    date = fields.Date(required=True)
+    duration_minutes = fields.Time(required=True)
+    notes = fields.Str()
