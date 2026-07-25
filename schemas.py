221f8910736486b4142d8d8b6ada09234cd938a5
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-07-25 23:15:52 +0300

    feat: add schemas

---
+++ schemas.py
@@ -0,0 +1,7 @@
+from marshmallow import Schema, ValidationError, fields, validates_schema
+
+class ExerciseSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True)
+    category = fields.Str(required=True)
+    equipment_needed = fields.Boolean(required=True)
