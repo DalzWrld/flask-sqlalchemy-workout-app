fbb3df8e4673c3a6f0d1a49ef6ecbf1be1d9a975
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-07-26 22:36:41 +0300

    feat: add exercises

--- resources/exercises.py
+++ resources/exercises.py
@@ -1,20 +1,24 @@
-from flask import make_response
+from flask import make_response, request
 from flask_restful import Resource
 
 from models import Exercise, db
+from schemas import exercise_schema, exercises_schema
 
 
 class Exercises(Resource):
     def get(self):
         exercises = Exercise.query.all()
-        return make_response([{"id": exercise.id, "name": exercise.name, "category": exercise.category} for exercise in exercises], 200)
+        return make_response(exercises_schema.dump(exercises), 200)
+
+    def post(self):
+        data = request.get_json()
 
 class ExerciseByID(Resource):
     def get(self, id):
         exercise = Exercise.query.filter_by(id=id).first()
         
         if exercise:
-            return make_response({"id": exercise.id, "name": exercise.name, "category": exercise.category}, 200)
+            return make_response(exercise_schema.dump(exercise), 200)
         else:
             response = {
                 "status": 404,
