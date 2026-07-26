38af1148202518ef51a0cc84571a6d4d19413982
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-07-26 22:50:23 +0300

    feat: add updated project setup

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, jsonify
 from flask_migrate import Migrate
 from flask_restful import Api
 
-from models import db
+from models import db, Workout, Exercise, WorkoutExercise
+from schemas import workout_schema, workouts_schema, exercise_schema, exercises_schema
 
 load_dotenv()
 
@@ -13,8 +14,14 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///demo.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app=app)
-
 migrate = Migrate(app=app, db=db)
+
+@app.route("/workouts", methods=["GET"])
+def get_workouts():
+    workouts = Workout.query.all()
+
+    return jsonify(workouts_schema.dump(workouts)), 200
+
 
 api = Api(app=app)
 
